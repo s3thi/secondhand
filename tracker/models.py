790de15cc1a1eb680af319346ef8f90cfe8caffc
Ankur Sethi <contact@ankursethi.in>
2013-03-12 13:59:55 +0530

    Add basic models.

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.DateTimeField()
+    user = models.ForeignKey(User)
+
+
+class WorkSession(models.Model):
+    task = models.ForeignKey('Task')
+    user = models.ForeignKey(User)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
