5cd430224cee01929088af3c7d40a87399f7ea4b
Ankur Sethi <contact@ankursethi.in>
2013-03-12 14:04:14 +0530

    Add URLs for the tracker API.

--- secondhand/urls.py
+++ secondhand/urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls import patterns, include, url
+from tastypie.api import Api
+from tracker.api import UserResource, TaskResource, WorkSessionResource
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'secondhand.views.home', name='home'),
-    # url(r'^secondhand/', include('secondhand.foo.urls')),
+# tracker API.
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(TaskResource())
+v1_api.register(WorkSessionResource())
 
+urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v1_api.urls())),
 )
