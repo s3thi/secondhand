f5fe7e5613255728c009f0c376c0077164baeaa6
Ankur Sethi <contact@ankursethi.in>
2013-03-12 20:42:41 +0530

    Added a RegistrationResource.

--- tracker/api.py
+++ tracker/api.py
@@ -1,13 +1,39 @@
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from tastypie import fields
+from tastypie.authentication import Authentication
+from tastypie.authorization import DjangoAuthorization, Authorization
+from tastypie.exceptions import BadRequest
 from tastypie.resources import ModelResource
 from tracker.models import Task, WorkSession
-from django.contrib.auth.models import User
-from tastypie import fields
 
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+        authorization = DjangoAuthorization()
+
+
+class RegistrationResource(ModelResource):
+    class Meta:
+        object_class = User
+        allowed_methods = ['post']
+        include_resource_uri = False
+        authentication = Authentication()
+        authorization = Authorization()
+    
+    def obj_create(self, bundle, request=None, **kwargs):
+        username = bundle.data['username']
+        email = bundle.data['email']
+        password = bundle.data['password']
+        
+        try:
+            bundle.obj = User.objects.create_user(username, email, password)
+        except IntegrityError:
+            raise BadRequest('Username exists.')
+        
+        return bundle
 
 
 class TaskResource(ModelResource):
@@ -16,6 +42,7 @@ class TaskResource(ModelResource):
     class Meta:
         queryset = Task.objects.all()
         resource_name = 'task'
+        authorization = DjangoAuthorization()
 
 
 class WorkSessionResource(ModelResource):
@@ -25,3 +52,4 @@ class WorkSessionResource(ModelResource):
     class Meta:
         queryset = WorkSession.objects.all()
         resource_name = 'work_session'
+        authorization = DjangoAuthorization()
