fe7663aca5bacf860cb31fbf43d80e4b76de9b92
Ankur Sethi <contact@ankursethi.in>
2013-03-13 11:01:59 +0530

    Add an ApiToken model.

--- tracker/models.py
+++ tracker/models.py
@@ -2,6 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class ApiToken(models.Model):
+    user = models.ForeignKey(User)
+    token = models.CharField(max_length=256)
+    generated_on = models.DateTimeField(auto_now_add=True)
+
+
 class Task(models.Model):
     name = models.DateTimeField()
     user = models.ForeignKey(User)
