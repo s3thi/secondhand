a181547b1288e896663dbf0cae9fcec1246936f2
Ankur Sethi <contact@ankursethi.in>
2013-03-17 14:06:48 +0530

    Add a console logger.

--- secondhand/settings.py
+++ secondhand/settings.py
@@ -138,7 +138,18 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
+    'formatters': {
+        'standard': {
+            'format' : '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt' : '%d/%b/%Y %H:%M:%S'
+        }
+    },
     'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
@@ -151,6 +162,10 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+        'tracker': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
     }
 }
 
